3970709a25a7967bb6bcabfaa7b5a38b20d34c22
Eddynesh <160381277+Eddynesh@users.noreply.github.com>
2024-10-09 15:49:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - www.codingnepalweb.com -->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Website Homepage HTML and CSS | CodingNepal</title>
+    <link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="style2.css">
+	
+	
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="navbar">
+        <h2 class="logo"><a href="#">tuko wifi</a></h2>
+        <input type="checkbox" id="menu-toggle" />
+        <label for="menu-toggle" id="hamburger-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <path d="M3 12h18M3 6h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </label>
+       <div class="buttons">
+          <a href="contact.php" class="signup">contact</a>
+        
+       
+          <a href="registration.php" class="signup">Sign Up</a>
+        </div>
+      </nav>
+    </header>
+    <section class="hero-section">
+      
+
+    <div class="wrapper">
+        <div class="card">
+            <h3>Bronze</h3>
+            <h1>Ksh.999 <span>/Month</span></h1>
+            <p>For Most Buisness that want to optimize their web queries.</p>
+            <ul>
+                <li><i class="fa-solid fa-square-check"></i> 8 MBPS</li>
+                <li><i class="fa-solid fa-square-check"></i> 24/7 sevice</li>
+                <li><i class="fa-solid fa-square-check"></i> Valid for 30 days</li>
+                <li><i class="fa-solid fa-square-check"></i> Unlimited Users</li>
+            </ul>
+            <a href="registration.php"">Choose Plan</a>
+
+        </div>
+
+        <div class="card">
+            <h3>Silver</h3>
+            <h1>Ksh.1,999 <span>/Month</span></h1>
+            <p>For Most Buisness that want to optimize their web queries.</p>
+            <ul>
+                <li><i class="fa-solid fa-square-check"></i> 15 MBPS</li>
+                <li><i class="fa-solid fa-square-check"></i> 24/7 sevice</li>
+                <li><i class="fa-solid fa-square-check"></i> Valid for 30 days</li>
+                <li><i class="fa-solid fa-square-check"></i> Unlimited Users</li>
+            </ul>
+            <a href="registration.php"">Choose Plan</a>
+
+        </div>
+
+
+        <div class="card">
+            <h3>GOLD</h3>
+            <h1>Ksh.3,999 <span>/Month</span></h1>
+            <p>For Most Buisness that want to optimize their web queries.</p>
+            <ul>
+                <li><i class="fa-solid fa-square-check"></i> 30 MBPS</li>
+                <li><i class="fa-solid fa-square-check"></i> 24/7 sevice</li>
+                <li><i class="fa-solid fa-square-check"></i> Valid for 30 days</li>
+                <li><i class="fa-solid fa-square-check"></i> Unlimited Users</li>
+            </ul>
+            <a href="registration.php"">Choose Plan</a>
+
+        </div>
+    </div>
+    
+      
+    </section>
+  </body>
+</html>
